ff4bb158e614de6d5f6a63a4855fa6d0e1513aa3
Jared99UwU <18030630@itcelaya.edu.mx>
2022-05-01 21:44:30 -0500

    Avance2

--- js/model.js
+++ js/model.js
@@ -2,6 +2,7 @@ export default class Model{
 	constructor(){
 		this.view=null;
 		this.todos = [];
+		this.currentId = 1;
 	}
 	
 	setView(view){
@@ -11,8 +12,40 @@ export default class Model{
 	getTodos(){
 		return this.todos;
 	}
+
+
+	findTOdO(id){
+  return this.tOdOs.findIndex((tOdO)=> tOdO.id === id);
+
+	}
+	toggleCompleted(id){
+
+	const index = this.findTOdO(id);
+	const tOdO = this.tOdOs[index];
+	tOdO.completed = !tOdO.completed;
+	console.log(this.tOdOs);
+
+	}
 	
 	addTodo(title,description){
-		console.log(title,description);
-	}
+		const tOdO={
+    
+			id:this.currentId++,
+            title,
+		    description,
+			completed: false,
+
+
+		}
+		this.todos.push(tOdO);
+		console.log(this.todos);
+
+		return{...tOdO};
+
+}
+ removeTOdO(id){
+
+	const index = this.findTOdO(id);
+	this.tOdOs.splice(index,1)
+}
 }
--- js/view.js
+++ js/view.js
@@ -1,18 +1,69 @@
+import addTodo from "./add-todo.js";
+
+
+
 export default class View{
 	constructor(){
 		this.model=null;
 		this.table=document.getElementById('table');
-		const btn = document.getElementById('add');
-		btn.onclick = function(){
-			this.addTodo('Titulo', 'Desc');
-		}
+		this.addTodoForm = new addTodo();
+
+		this.addTodoForm.onClick((title, description) => this.addTodo(title, description));
+		
 	}
 	
 	setModel(model){
 		this.model=model;
 	}
+
+
 	
 	addTodo(title, description){
+		const tOdO = this.model.addTodo(title, description);
+		this.createRow(tOdO);
+	}
+
+	toggleCompleted(id){
+
+		this.model.toggleCompleted(id);
+	}
+
+removeTodo(id){
+	this.model.removeTodo(id);
+	document.getElementById(id).remove();
+	
+}
+
+	createRow(tOdO){
+		
+		const row = table.insertRow();
+		row.setAttribute('id', tOdO.id);
+		row.innerHTML =  `
+		<td>${tOdO.title}</td>
+		<td>${tOdO.description}</td>
+		<td class="text-center">
+		  
+		</td>
+		<td class="text-right">
+		<button class="btn btn-primary mb-1">
+		  <i class="fa fa-pencil"></i>
+		</button>
+		</td>
+	`;
+
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	checkbox.checked = tOdO.completed;
+	checkbox.onclick = () => this.toggleCompleted(tOdO.id);
+	row.children[2].appendChild(checkbox);
+		
+	
+	const removeBtn = document.createElement('button');
+	removeBtn.classList.add('btn','btn-danger', 'mb-1', 'ml-1');
+	removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
+	removeBtn.onclick = () => this.removeTodo(tOdO.id);
+	
+	row.children[3].appendChild(removeBtn);
 		
 	}
 }

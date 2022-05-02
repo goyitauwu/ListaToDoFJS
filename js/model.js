a827999995b0753c3a941e1bcc62a21f425e2639
Ferquivi <102878032+Ferquivi@users.noreply.github.com>
2022-05-02 09:05:52 -0500

    confirmar

--- js/model.js
+++ js/model.js
@@ -16,52 +16,54 @@ export default class Model {
 		this.currentId = this.todos[this.todos.length - 1].id + 1;
 	  }
 	}
-	
-	setView(view){
-		this.view=view;
+  
+	setView(view) {
+	  this.view = view;
 	}
-	
+  
 	save() {
-		localStorage.setItem('todos', JSON.stringify(this.todos));
+	  localStorage.setItem('todos', JSON.stringify(this.todos));
+	}
+  
+	getTodos() {
+	  return this.todos.map((todo) => ({...todo}));
+	}
+  
+	findTodo(id) {
+	  return this.todos.findIndex((todo) => todo.id === id);
+	}
+  
+	toggleCompleted(id) {
+	  const index = this.findTodo(id);
+	  const todo = this.todos[index];
+	  todo.completed = !todo.completed;
+	  this.save();
+	}
+  
+	editTodo(id, values) {
+	  const index = this.findTodo(id);
+	  Object.assign(this.todos[index], values);
+	  this.save();
+	}
+  
+	addTodo(title, description) {
+	  const todo = {
+		id: this.currentId++,
+		title,
+		description,
+		completed: false,
 	  }
-
-	getTodos(){
-		return this.todos;
+  
+	  this.todos.push(todo);
+	  console.log(this.todos);
+	  this.save();
+  
+	  return {...todo};
 	}
-
-
-	findTOdO(id){
-  return this.tOdOs.findIndex((tOdO)=> tOdO.id === id);
-
+  
+	removeTodo(id) {
+	  const index = this.findTodo(id);
+	  this.todos.splice(index, 1);  
+	  this.save();
 	}
-	toggleCompleted(id){
-
-	const index = this.findTOdO(id);
-	const tOdO = this.tOdOs[index];
-	tOdO.completed = !tOdO.completed;
-	console.log(this.tOdOs);
-
-	}
-	
-	addTodo(title,description){
-		const tOdO={
-    
-			id:this.currentId++,
-            title,
-		    description,
-			completed: false,
-
-
-		}
-		this.todos.push(tOdO);
-		console.log(this.todos);
-
-		return{...tOdO};
-
-}
- removeTOdO(id){
-
-	const index = this.findTOdO(id);
-	this.tOdOs.splice(index,1)
-}
-}
+  }

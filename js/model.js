5a438f28269fce44fbb27995a874d0bd877492d1
Ferquivi <102878032+Ferquivi@users.noreply.github.com>
2022-05-02 09:00:15 -0500

    confirmar

--- js/model.js
+++ js/model.js
@@ -1,8 +1,20 @@
-export default class Model{
-	constructor(){
-		this.view=null;
-		this.todos = [];
+export default class Model {
+	constructor() {
+	  this.view = null;
+	  this.todos = JSON.parse(localStorage.getItem('todos'));
+	  if (!this.todos || this.todos.length < 1) {
+		this.todos = [
+		  {
+			id: 0,
+			title: 'Learn JS',
+			description: 'Watch JS Tutorials',
+			completed: false,
+		  }
+		]
 		this.currentId = 1;
+	  } else {
+		this.currentId = this.todos[this.todos.length - 1].id + 1;
+	  }
 	}
 	
 	setView(view){
